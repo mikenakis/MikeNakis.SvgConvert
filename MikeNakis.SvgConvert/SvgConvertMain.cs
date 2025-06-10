329edc0d8d1c29d6fe67b98b9b384b49b6283275
MikeNakis <git@michael.gr>
2025-06-10 10:46:11 +0200

    refactoring

--- MikeNakis.SvgConvert/SvgConvertMain.cs
+++ MikeNakis.SvgConvert/SvgConvertMain.cs
@@ -19,65 +19,57 @@ sealed class Svg2IcoMain
 	static int run( string[] arguments )
 	{
 		Clio.ArgumentParser argumentParser = new();
-		Clio.IVerbArgument watchVerb = argumentParser.AddVerb( "png", description: "Converts to PNG", convertToPngHandler );
-		Clio.IVerbArgument parseVerb = argumentParser.AddVerb( "windows-ico", description: "Converts to windows-ICO", convertToWindowsIcoHandler );
+		Clio.IVerbArgument watchVerb = argumentParser.AddVerb( "png", description: "Converts to PNG", argumentParser =>
+		{
+			Clio.IOptionArgument<string?> widthArgument = argumentParser.AddStringOption( "width", 'w', "The width of the generated PNG file.", "pixels" );
+			Clio.IOptionArgument<string?> heightArgument = argumentParser.AddStringOption( "height", 'h', "The height of the generated PNG file.", "pixels" );
+			Clio.IPositionalArgument<string> inputFileArgument = argumentParser.AddRequiredStringPositional( "input-file", "The SVG file to read" );
+			Clio.IPositionalArgument<string?> outputFileArgument = argumentParser.AddStringPositional( "output-file", "The PNG file to write" );
+			if( !argumentParser.TryParse() )
+				return;
+			FilePath inputFilePath = FilePath.FromRelativeOrAbsolutePath( inputFileArgument.Value );
+			FilePath outputFilePath = getPngFilePath( inputFilePath, outputFileArgument.Value );
+			Log.Debug( $"Reading {inputFilePath}" );
+			Svg.SvgDocument svgDocument = readSvgDocument( inputFilePath );
+			int width = widthArgument.Value == null ? (int)svgDocument.Bounds.Width : parseInt( widthArgument.Value );
+			int height = heightArgument.Value == null ? (int)svgDocument.Bounds.Height : parseInt( heightArgument.Value );
+			convertToPng( inputFilePath, outputFilePath, width, height );
+
+			static FilePath getPngFilePath( FilePath svgFilePath, string? pngFileArgumentValue )
+				=> getRelatedFilePath( svgFilePath, pngFileArgumentValue, ".png" );
+		} );
+		Clio.IVerbArgument parseVerb = argumentParser.AddVerb( "windows-ico", description: "Converts to windows-ICO", argumentParser =>
+		{
+			Clio.IOptionArgument<string> iconSizesArgument = argumentParser.AddStringOptionWithDefault( "icon-sizes", "16,32,48,64,128,256", 's', "A comma-separated list of icon sizes to create.", "sizes" );
+			Clio.IPositionalArgument<string> inputFileArgument = argumentParser.AddRequiredStringPositional( "input-file", "The SVG file to read" );
+			Clio.IPositionalArgument<string?> outputFileArgument = argumentParser.AddStringPositional( "output-file", "The windows-ICO file to write" );
+			if( !argumentParser.TryParse() )
+				return;
+			IReadOnlyList<int> iconSizes = iconSizesArgument.Value.Split( ',' ).Select( int.Parse ).Collect();
+			FilePath inputFilePath = FilePath.FromRelativeOrAbsolutePath( inputFileArgument.Value );
+			FilePath outputFilePath = getIcoFilePath( inputFilePath, outputFileArgument.Value );
+			convertToWindowsIco( inputFilePath, outputFilePath, iconSizes );
+
+			static FilePath getIcoFilePath( FilePath svgFilePath, string? icoFileArgumentValue )
+				=> getRelatedFilePath( svgFilePath, icoFileArgumentValue, ".ico" );
+		} );
 		if( !argumentParser.TryParse( arguments ) )
 			return -1;
 		return 0;
 	}
 
-	static void convertToPngHandler( Clio.ChildArgumentParser argumentParser )
+	static void convertToPng( FilePath inputFilePath, FilePath outputFilePath, int width, int height )
 	{
-		Clio.IOptionArgument<string?> widthArgument = argumentParser.AddStringOption( "width", 'w', "The width of the generated PNG file.", "pixels" );
-		Clio.IOptionArgument<string?> heightArgument = argumentParser.AddStringOption( "height", 'h', "The height of the generated PNG file.", "pixels" );
-		Clio.IPositionalArgument<string> inputFileArgument = argumentParser.AddRequiredStringPositional( "input-file", "The SVG file to read" );
-		Clio.IPositionalArgument<string?> outputFileArgument = argumentParser.AddStringPositional( "output-file", "The PNG file to write" );
-		if( !argumentParser.TryParse() )
-			return;
-		FilePath inputFilePath = FilePath.FromRelativeOrAbsolutePath( inputFileArgument.Value );
-		FilePath outputFilePath = getPngFilePath( inputFilePath, outputFileArgument.Value );
 		Log.Debug( $"Reading {inputFilePath}" );
 		Svg.SvgDocument svgDocument = readSvgDocument( inputFilePath );
-		int width = widthArgument.Value == null ? (int)svgDocument.Bounds.Width : parseInt( widthArgument.Value );
-		int height = heightArgument.Value == null ? (int)svgDocument.Bounds.Height : parseInt( heightArgument.Value );
 		Log.Debug( $"Writing {outputFilePath}" );
 		SysDraw.Bitmap bitmap = svgDocument.Draw( width, height );
 		bitmap.Save( outputFilePath.Path, SysImage.ImageFormat.Png );
 		return;
-
-		static FilePath getPngFilePath( FilePath svgFilePath, string? pngFileArgumentValue )
-			=> getRelatedFilePath( svgFilePath, pngFileArgumentValue, ".png" );
 	}
 
-	static int parseInt( string s )
+	static void convertToWindowsIco( FilePath inputFilePath, FilePath outputFilePath, IReadOnlyList<int> iconSizes )
 	{
-		//PEARL: When `int.Parse()` fails, it throws a `System.FormatException`.
-		//    This absolutely retarded exception contains no fields other than the message,
-		//    and the message says nothing but "Input string was not in a correct format."
-		//    So, it fails to convey two very important pieces of information:
-		//      1. What was the string that was not in a correct format.
-		//      2. What was the correct format.
-		//    We fix this insanity here.
-		try
-		{
-			return int.Parse( s, SysGlob.CultureInfo.InvariantCulture );
-		}
-		catch( Sys.Exception exception )
-		{
-			throw new Sys.InvalidOperationException( $"Failed to parse '{s}' as an integer", exception );
-		}
-	}
-
-	static void convertToWindowsIcoHandler( Clio.ChildArgumentParser argumentParser )
-	{
-		Clio.IOptionArgument<string> iconSizesArgument = argumentParser.AddStringOptionWithDefault( "icon-sizes", "16,32,48,64,128,256", 's', "A comma-separated list of icon sizes to create.", "sizes" );
-		Clio.IPositionalArgument<string> inputFileArgument = argumentParser.AddRequiredStringPositional( "input-file", "The SVG file to read" );
-		Clio.IPositionalArgument<string?> outputFileArgument = argumentParser.AddStringPositional( "output-file", "The windows-ICO file to write" );
-		if( !argumentParser.TryParse() )
-			return;
-		IReadOnlyList<int> iconSizes = iconSizesArgument.Value.Split( ',' ).Select( int.Parse ).Collect();
-		FilePath inputFilePath = FilePath.FromRelativeOrAbsolutePath( inputFileArgument.Value );
-		FilePath outputFilePath = getIcoFilePath( inputFilePath, outputFileArgument.Value );
 		Log.Debug( $"Reading {inputFilePath}" );
 		Svg.SvgDocument svgDocument = readSvgDocument( inputFilePath );
 		IReadOnlyList<SysDraw.Image> images = createImages( svgDocument, iconSizes );
@@ -85,9 +77,6 @@ sealed class Svg2IcoMain
 		Log.Debug( $"Writing {outputFilePath}" );
 		saveIcon( outputFilePath, icon );
 		return;
-
-		static FilePath getIcoFilePath( FilePath svgFilePath, string? icoFileArgumentValue )
-			=> getRelatedFilePath( svgFilePath, icoFileArgumentValue, ".ico" );
 
 		static IReadOnlyList<SysDraw.Image> createImages( Svg.SvgDocument svgDocument, IReadOnlyList<int> iconSizes )
 		{
@@ -158,5 +147,24 @@ sealed class Svg2IcoMain
 		if( targetFileName.EndsWith( '/' ) || targetFileName.EndsWith( '\\' ) || targetDirectoryPath.Exists() )
 			return FilePath.Of( targetDirectoryPath, sourceFilePath.GetFileNameWithoutExtension() + extension );
 		return FilePath.FromRelativeOrAbsolutePath( targetFileName );
+	}
+
+	static int parseInt( string s )
+	{
+		//PEARL: When `int.Parse()` fails, it throws a `System.FormatException`.
+		//    This absolutely retarded exception contains no fields other than the message,
+		//    and the message says nothing but "Input string was not in a correct format."
+		//    So, it fails to convey two very important pieces of information:
+		//      1. What was the string that was not in a correct format.
+		//      2. What was the correct format.
+		//    We fix this insanity here.
+		try
+		{
+			return int.Parse( s, SysGlob.CultureInfo.InvariantCulture );
+		}
+		catch( Sys.Exception exception )
+		{
+			throw new Sys.InvalidOperationException( $"Failed to parse '{s}' as an integer", exception );
+		}
 	}
 }
