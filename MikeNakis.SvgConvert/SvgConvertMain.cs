464f66c63751ec5adbffec5453126cd26fb0fa90
MikeNakis <git@michael.gr>
2026-04-30 22:22:40 +0200

    refactoring, bugfixes, renamed project 'Coloring' to 'Colors', introduced project 'Colors Test'.

--- MikeNakis.SvgConvert/SvgConvertMain.cs
+++ MikeNakis.SvgConvert/SvgConvertMain.cs
@@ -85,7 +85,7 @@ sealed class Svg2IcoMain
 		if( maybeWidth.HasValue && maybeHeight.HasValue )
 		{
 			double requestedAspectRatio = maybeWidth.Value / maybeHeight.Value;
-			if( !NearlyEquals( aspectRatio, requestedAspectRatio ) )
+			if( !aspectRatio.NearlyEquals( requestedAspectRatio ) )
 				throw new GenericException( $"SVG aspect ratio is {aspectRatio}, requested width {maybeWidth.Value} and height {maybeHeight.Value} have a different aspect ratio." );
 			width = maybeWidth.Value;
 			height = maybeHeight.Value;
